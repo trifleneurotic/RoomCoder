bb36823e96ded6ee3682454b004ed7610456caf7
G Stone <gps@trifleneurotic.com>
2023-12-16 12:39:26 -0800

    Aligning migrations again.

--- Program.cs
+++ Program.cs
@@ -29,7 +29,6 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
 	db.Database.EnsureDeleted();
-	db.Database.EnsureCreated();
     db.Database.Migrate();
 }
 
